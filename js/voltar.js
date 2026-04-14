53cabbee49abc0a11b7ae45add0d0ecd2bf09d71
pedrozoka141 <pedrobznss@gmail.com>
2026-04-14 15:14:37 -0300

    Refactor voltarJogo and gerarHash functions

--- js/voltar.js
+++ js/voltar.js
@@ -25,8 +25,8 @@ async function voltarJogo() {
         "e14c9927c8235fce2e5d08b23eb8fd9d1b2c703c0338fc9b674bbfc633458802": "ZGlhcmlvLmh0bWw=", // 06
         "4aad5a7e7b58ee08ac9646a0ff375edcdafb0d9dcb72af7ff82d76519cbabb87": "YmFieS5odG1s", // 07
         "16a2dd3a872593902a1e39276f1800bdb42614ff16caa364938e35ed8d8326a9": "aW52ZXJ0YS5odG1s", // 08
-        "1d5a04fbeace6278ac78229ccac4064ede1f7c62ce18b6989e72a52ff99c9259": "ZXNwYcOnby5odG1s", // 09   
-        "538d7d9fe78e7baac47a9fbd6f2c68845ecca72dbdc2b47b4c5a0f5620ab8e93": "zpXOu867zqzOtM6xLmh0bWw=", // 10
+        "1d5a04fbeace6278ac78229ccac4064ede1f7c62ce18b6989e72a52ff99c9259": "ZXNwYWNvLmh0bWw", // 09   
+        "538d7d9fe78e7baac47a9fbd6f2c68845ecca72dbdc2b47b4c5a0f5620ab8e93": "aGVsYWRlLmh0bWw=", // 10
         "31edb26fe93d2b781230e6d3aba757c5049d2fbcf388c29565cab911ef0098f0": "ZnJlcXVlbmNpYS5odG1s", // 11
         "0b97d878aae572c71685cd7a2df47958b5ee7069c95c022da82d8fb0dd99f1b3": "cHJpbWVpcmFjYXJ0YS5odG1s", // 12
         "1367a84fc8304db903653a95a9b071bfc0607bb93d837cc562877555e6ea17ed": "bGF0aW0uaHRtbA==", // 13
@@ -34,7 +34,7 @@ async function voltarJogo() {
         "0a2c0cff9c19a1f3a0edc6d86510cd7bbc7de676c8dc29523866eb52582e2cc0": "bmVyZHRoaW5ncy5odG1s", // 15
         "d4d38dd62b7af86a945668d8bfc7e12a38442edc7cf87a1facd689d5e2a2b4a3": "S0QuaHRtbA==", // 16
         "763a87e8ecd542b4690f0eb87ee2f7b2955ba3db89bda3350167e589b38653fe": "eGVub2ZvbnRlLmh0bWw=", // 17
-        "412b0d878f7cc81d8a61d5b45669baf3712591f1785a321f0501da4151a0192b": "zrHOsi5odG1s", // 18
+        "412b0d878f7cc81d8a61d5b45669baf3712591f1785a321f0501da4151a0192b": "YWxmYS1iZXRhLmh0bWw", // 18
         "8323e170e90cfe7486abbd05d7153d6673bfd0cb3fe5729794890521b7d1cd5a": "bXVsdGlwbGlxdWUuaHRtbA==", // 19
         "25f63ce0a144f4f4082bc843ab550656d0ad49e83cca0b1792d14df23e4f5ef7": "d2UtYXJlLWZhbWlseS5odG1s", // 20
         "b2471d941bf888366cf43813752ea2ebfef08254773cf116187cdd6d0463a50a": "QjE2Lmh0bWw=", // 21
